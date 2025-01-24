182ff421cac16cca55fcda7fc1f2f8d368d6772d
marcopetry <marcomattospetry@gmail.com>
2025-01-24 15:16:17 -0300

    refactor: transform file in ts

--- data-structure/linked-list/index.ts
+++ data-structure/linked-list/index.ts
@@ -1,25 +1,31 @@
 class Node {
+  public value: number;
+  public next: Node | null;
   constructor(value) {
     this.value = value;
     this.next = null;
   }
 }
 
-class LinkedList {
-  constructor(value) {
+export class LinkedList {
+  public head: Node | null;
+  public tail: Node | null;
+  public length: number;
+
+  constructor(value?: number) {
     this.head = new Node(value);
     this.tail = this.head;
     this.length = 1;
   }
 
-  push(value) {
+  push(value: number) {
     let newNode = new Node(value);
 
     if (this.length === 0) {
       this.head = newNode;
       this.tail = newNode;
     } else {
-      this.tail.next = newNode;
+      this.tail!.next = newNode;
       this.tail = newNode;
     }
 
