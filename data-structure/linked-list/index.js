296d8d18dab131175e781f734ce7806e6eadd7df
marcopetry <marcomattospetry@gmail.com>
2025-01-24 09:55:06 -0300

    feat: add push method inlinked list

--- data-structure/linked-list/index.js
+++ data-structure/linked-list/index.js
@@ -11,4 +11,19 @@ class LinkedList {
     this.tail = this.head;
     this.length = 1;
   }
+
+  push(value) {
+    let newNode = new Node(value);
+
+    if (this.length === 0) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+
+    this.length++;
+    return this;
+  }
 }
